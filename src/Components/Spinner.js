29cb8daf0f58d047f6d8f1b33421212ea5c7c0df
Suraj Kumar Singh <53943973+SurajSingh2159@users.noreply.github.com>
2023-03-20 12:45:17 +0530

    final commit

---
+++ src/Components/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Spinner = ()=> {
+    return (
+      <div>
+        <div class="text-center">
+            <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+            </div>
+       </div>
+      </div>
+    )
+  
+}
+
+export default Spinner
